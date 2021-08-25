496745ac0ec8424a9c1a47afb14ba0de3bc36378
veronica <verovargaszaraza@gmail.com>
2021-08-24 21:48:56 -0400

    add logic behind answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
-    @question = ''
+    @questions = ''
   end
+
   def answer
-    @question = ''
+    @question = params[:coach_ask]
+
+    @answer = if @question.include? '?'
+                'Silly question, get dressed and go to work!'
+              elsif @question == 'I am going to work right now!'
+                'Great!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
